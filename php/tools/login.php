3543959874da3036aa5830dbb7dd2e8b507dd53a
GamingLegion <jacobdgrieco@gmail.com>
2024-04-08 15:55:08 -0400

    connected to SQL for user confirmation

--- php/tools/login.php
+++ php/tools/login.php
@@ -1,16 +1,17 @@
 <?php
-// Simulated user data (Replace with actual user authentication logic)
-$valid_username = "admin";
-$valid_password = "password";
+$connect = mysqli_connect( 'localhost', 'root', 'theallseeingeyes', 'punkcloud' );
 
-// Retrieve username and password from the request
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = $_POST[ 'username' ];
+$password = $_POST[ 'password' ];
+
+$result = mysqli_query( $connect, 'Select username, email, password FROM users' );
 
 // Check if the submitted username and password match the valid credentials
-if ($username === $valid_username && $password === $valid_password) {
-    echo "success"; // Return success message if login is successful
-} else {
-    echo "error"; // Return error message if login is unsuccessful
+
+while ( $record = mysqli_fetch_assoc( $result ) ) {
+   if ( ($username === $record['username'] || $username === $record['email']) && $password === $record['password'] ) {
+      echo "success";
+   }
 }
+
 ?>
