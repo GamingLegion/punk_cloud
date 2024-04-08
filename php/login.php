9bed2d59e10fc14adf0a2f43f940ca8fc9ce6b14
GamingLegion <jacobdgrieco@gmail.com>
2024-04-08 15:55:18 -0400

    added Vegapunk image

--- php/login.php
+++ php/login.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="../css/login.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Login</title>
+<link rel="stylesheet" href="../css/login.css">
 </head>
 <body>
-    <div class="container">
-        <h2>Login</h2>
-        <form id="loginForm" action="tools/login.php" method="post">
-            <div class="form-group">
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <div class="form-group">
-                <button type="submit">Login</button>
-            </div>
-            <p id="error-msg"></p>
-        </form>
-    </div>
-    <script src="../js/login.js"></script>
+   <img src="../images/icons/vegapunk.png" alt="Vegapunk" style="position: fixed; width: 15%; margin: 100px 0px 0px 150px">
+<div class="container">
+   <h2>Login</h2>
+   <form id="loginForm" action="tools/login.php" method="post">
+      <div class="form-group">
+         <label for="username">User/Email:</label>
+         <input type="text" id="username" name="username" required>
+      </div>
+      <div class="form-group">
+         <label for="password">Password:</label>
+         <input type="password" id="password" name="password" required>
+      </div>
+      <div class="form-group">
+         <button type="submit">Login</button>
+      </div>
+      <p id="error-msg"></p>
+   </form>
+   <p>Sign Up</p>
+</div>
+<script src="../js/login.js"></script>
 </body>
 </html>
