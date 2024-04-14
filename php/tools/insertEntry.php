b43deebae393d69556ed738a5400064695a2fe88
GamingLegion <jacobdgrieco@gmail.com>
2024-04-14 14:45:49 -0400

    adjusted for correct time insertion and entry type

--- php/tools/insertEntry.php
+++ php/tools/insertEntry.php
@@ -2,19 +2,23 @@
 $connect = mysqli_connect( 'localhost', 'root', 'theallseeingeyes', 'punkcloud' );
 
 if ( isset( $_POST[ 'submit' ] ) ) {
-	$ins_date = date("Y-m-d H:i:s");
-  $eng_name = $_POST[ 'eng_name' ];
-  $rom_name = $_POST[ 'rom_name' ];
-  $image = $_POST[ 'image' ];
+   date_default_timezone_set( 'America/New_York' );
+   $ins_date = date( "Y-m-d H:i:s" );
+   $eng_name = $_POST[ 'eng_name' ];
+   $rom_name = $_POST[ 'rom_name' ];
+   $entry_type = $_POST[ 'entry_type' ];
+   $image = $_POST[ 'image' ];
 
-  $eng_name = str_replace( "'", "\'", $eng_name );
-  $eng_name = str_replace( "\"", "\"", $eng_name );
-  $rom_name = str_replace( "'", "\'", $rom_name );
-  $rom_name = str_replace( "\"", "\"", $rom_name );
+   if ( $eng_name !== NULL && $rom_name !== NULL ) {
+      $eng_name = str_replace( "'", "\'", $eng_name );
+      $eng_name = str_replace( "\"", "\"", $eng_name );
+      $rom_name = str_replace( "'", "\'", $rom_name );
+      $rom_name = str_replace( "\"", "\"", $rom_name );
 
-  mysqli_query( $connect, "INSERT INTO anime_shows(id, ins_date, upd_date, eng_name, rom_name, image)
-	VALUES ('NULL', '$ins_date', '$ins_date', '$eng_name', '$rom_name', '$image');");
-   mysqli_close( $connect );
-   header( "Location: http://localhost/PunkCloud/php/home.php" );
+      mysqli_query( $connect, "INSERT INTO $entry_type(id, ins_date, upd_date, eng_name, rom_name, image)
+	VALUES ('NULL', '$ins_date', '$ins_date', '$eng_name', '$rom_name', '$image');" );
+      mysqli_close( $connect );
+      header( "Location: http://localhost/PunkCloud/php/home.php" );
+   }
 }
 ?>
