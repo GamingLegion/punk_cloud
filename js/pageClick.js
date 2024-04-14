b1549d0de2f463d8bb14183e41ea151bba949a9d
GamingLegion <jacobdgrieco@gmail.com>
2024-04-14 17:52:17 -0400

    changed to work with the title, which was set to the romanized version for proper population of animePage

--- js/pageClick.js
+++ js/pageClick.js
@@ -4,7 +4,7 @@ var divs = document.querySelectorAll(".aniCard");
 
 divs.forEach(function (div) {
    div.addEventListener("click", function () {
-      var name = this.querySelector("p").innerText;
+      var name = this.querySelector("p").title;
       var link = "http://localhost/PunkCloud/php/animePage.php?link=" + name;
       window.location.href = link;
    });
