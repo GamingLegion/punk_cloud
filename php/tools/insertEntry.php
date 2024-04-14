44d2548625f4e338f391d43d9ffa27b07fb9072e
GamingLegion <jacobdgrieco@gmail.com>
2024-04-14 17:56:54 -0400

    added conditions for empty fields

--- php/tools/insertEntry.php
+++ php/tools/insertEntry.php
@@ -4,19 +4,19 @@ $connect = mysqli_connect( 'localhost', 'root', 'theallseeingeyes', 'punkcloud' 
 if ( isset( $_POST[ 'submit' ] ) ) {
    date_default_timezone_set( 'America/New_York' );
    $ins_date = date( "Y-m-d H:i:s" );
-   $eng_name = $_POST[ 'eng_name' ];
    $rom_name = $_POST[ 'rom_name' ];
-   $entry_type = $_POST[ 'entry_type' ];
-   $image = $_POST[ 'image' ];
+   $eng_name = ( $_POST[ 'eng_name' ] !== "" ) ? $_POST[ 'eng_name' ] : NULL;
+   $image = ( $_POST[ 'image' ] !== "" ) ? $_POST[ 'image' ] : 'deafult.png';
 
-   if ( $eng_name !== NULL && $rom_name !== NULL ) {
-      $eng_name = str_replace( "'", "\'", $eng_name );
-      $eng_name = str_replace( "\"", "\"", $eng_name );
+   if ( $rom_name !== NULL ) {
+      if ( $eng_name !== NULL ) {
+         $eng_name = str_replace( "'", "\'", $eng_name );
+         $eng_name = str_replace( "\"", "\"", $eng_name );
+      }
       $rom_name = str_replace( "'", "\'", $rom_name );
       $rom_name = str_replace( "\"", "\"", $rom_name );
 
-      mysqli_query( $connect, "INSERT INTO $entry_type(id, ins_date, upd_date, eng_name, rom_name, image)
-	VALUES ('NULL', '$ins_date', '$ins_date', '$eng_name', '$rom_name', '$image');" );
+      mysqli_query( $connect, "INSERT INTO $entry_type(id, ins_date, upd_date, eng_name, rom_name, image) VALUES ('NULL', '$ins_date', '$ins_date', '$eng_name', '$rom_name', '$image');" );
       mysqli_close( $connect );
       header( "Location: http://localhost/PunkCloud/php/home.php" );
    }
