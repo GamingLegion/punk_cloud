e08d91e3553e9f340a9866d2c19fc3b073947cf1
GamingLegion <jacobdgrieco@gmail.com>
2024-04-14 14:09:33 -0400

    shaved down insert information

--- php/addEntry.php
+++ php/addEntry.php
@@ -14,32 +14,6 @@
 		<label>English Name: </label><input type="text" name="eng_name"><br>
 		<label>Romanized Name: </label><input type="text" name="rom_name"><br>
 		<label>Image Path: </label><input type="file" name="image"><br>
-		<label>Status: </label><select name="status">
-			<option>Unaired</option>
-			<option>Airing</option>
-			<option>Completed</option>
-		</select><br>
-		<label>Episode Number: </label><input type="number" name="epi_num"><br>
-		<label>Start Date: </label><input type="date" name="start_date"><br>
-		<label>End Date: </label><input type="date" name="end_date"><br>
-		<label>Air Season: </label><select name="air_season">
-			<option></option>
-			<option>Summer 2024</option>
-			<option>Spring 2024</option>
-			<option>Winter 2024</option>
-			<option>Fall 2023</option>
-			<option>Summer 2023</option>
-			<option>Spring 2023</option>
-			<option>Winter 2023</option>
-		</select><br>
-		<label>Demographic: </label><select name="demographic">
-			<option></option>
-			<option>Josei</option>
-			<option>Kids</option>
-			<option>Seinen</option>
-			<option>Shoujo</option>
-			<option>Shounen</option>
-		</select><br><br>
 		<button type="submit" name="submit">Submit</button>
 	</form>
 </body>
--- php/tools/insertEntry.php
+++ php/tools/insertEntry.php
@@ -6,19 +6,15 @@ if ( isset( $_POST[ 'submit' ] ) ) {
   $eng_name = $_POST[ 'eng_name' ];
   $rom_name = $_POST[ 'rom_name' ];
   $image = $_POST[ 'image' ];
-  $status = strtolower( $_POST[ 'status' ] );
-  $epi_num = $_POST[ 'epi_num' ];
-  $start_date = $_POST[ 'start_date' ];
-  $end_date = $_POST[ 'end_date' ];
-  $air_season = $_POST[ 'air_season' ];
-  $demographic = $_POST[ 'demographic' ];
 
   $eng_name = str_replace( "'", "\'", $eng_name );
   $eng_name = str_replace( "\"", "\"", $eng_name );
   $rom_name = str_replace( "'", "\'", $rom_name );
   $rom_name = str_replace( "\"", "\"", $rom_name );
 
-  mysqli_query( $connect, "INSERT INTO anime_shows(id, ins_date, upd_date, eng_name, rom_name, image, status, epi_num, start_date, end_date, air_season, demographic)
-	VALUES ('NULL', '$ins_date', '$ins_date', '$eng_name', '$rom_name', '$image', '$status', '$epi_num', '$start_date', '$end_date', '$air_season', '$demographic');");
+  mysqli_query( $connect, "INSERT INTO anime_shows(id, ins_date, upd_date, eng_name, rom_name, image)
+	VALUES ('NULL', '$ins_date', '$ins_date', '$eng_name', '$rom_name', '$image');");
+   mysqli_close( $connect );
+   header( "Location: http://localhost/PunkCloud/php/home.php" );
 }
 ?>
