82336fec99b9f66e36326834bf5681c40d2e0538
GamingLegion <jacobdgrieco@gmail.com>
2024-04-14 14:53:38 -0400

    cleaned up code

--- js/pageClick.js
+++ js/pageClick.js
@@ -2,12 +2,10 @@
 
 var divs = document.querySelectorAll(".aniCard");
 
-// Loop through each div and attach a click event listener
-divs.forEach(function(div) {
-    div.addEventListener("click", function() {
-		var name = this.querySelector("p").innerText;
-		var link = "http://localhost/PunkCloud/php/animePage.php?link=" + name;
-
-        window.location.href = link; // Redirect to the specified URL
-    });
+divs.forEach(function (div) {
+   div.addEventListener("click", function () {
+      var name = this.querySelector("p").innerText;
+      var link = "http://localhost/PunkCloud/php/animePage.php?link=" + name;
+      window.location.href = link;
+   });
 });
