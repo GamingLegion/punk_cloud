eecdd04bbe6712d0c937a281289b759b1dce8c9f
GamingLegion <jacobdgrieco@gmail.com>
2024-04-14 23:45:52 -0400

    updated for changed tags

--- js/editAnime.js
+++ js/editAnime.js
@@ -1,70 +1,19 @@
 //JavaScript Document
 var infoLines = document.querySelectorAll('.info_line');
 infoLines.forEach(function (infoLine) {
-   var firstParagraph = infoLine.querySelector('p');
-   var secondParagraph = infoLine.querySelectorAll('p')[1];
+   var firstParagraph = infoLine.querySelector('a');
+   var secondParagraph = infoLine.querySelectorAll('a')[1];
    var editButton = document.createElement('button');
    editButton.id = 'editBtn';
    editButton.textContent = 'Edit';
    editButton.onclick = function () {
-      var newValue;
-      if (secondParagraph.id === 'height_ft') {
-         newValue = prompt('Enter new value for ' + secondParagraph.id, '');
+      var newValue= prompt('Enter new value for ' + secondParagraph.id, secondParagraph.textContent);
          if (newValue !== null) {
             updateField(secondParagraph.id, newValue);
          }
-      } else {
-         newValue = prompt('Enter new value for ' + secondParagraph.id, secondParagraph.textContent);
-         if (newValue !== null) {
-            updateField(secondParagraph.id, newValue);
-         }
-      }
-
-      if (secondParagraph.id === 'height_ft' || secondParagraph.id === 'measurements_bust') {
-         var thirdParagraph = infoLine.querySelectorAll('p')[2];
-         newValue = prompt('Enter new value for ' + thirdParagraph.id, '');
-         if (newValue !== null) {
-            updateField(thirdParagraph.id, newValue);
-         }
-      }
-
-      if (secondParagraph.id === 'measurements_bust') {
-         var fourthParagraph = infoLine.querySelectorAll('p')[3];
-         newValue = prompt('Enter new value for ' + fourthParagraph.id, '');
-         if (newValue !== null) {
-            updateField(fourthParagraph.id, newValue);
-         }
-      }
    };
 
    infoLine.insertBefore(editButton, firstParagraph);
-});
-
-var natiTits = document.querySelector('#nati_tit');
-natiTits.addEventListener("click", function () {
-   if (this.getAttribute('src') === "../images/icons/soa.png") {
-      this.src = "../images/icons/no_soa.png";
-      updateField('natural_tits', 0);
-   } else {
-      this.src = "../images/icons/soa.png";
-      updateField('natural_tits', 1);
-   }
-});
-
-var offWebs = document.querySelectorAll('.offWebs');
-offWebs.forEach(function (div) {
-   var header = div.querySelector('h2');
-   var editButton = document.createElement('button');
-   editButton.id = 'editBtn';
-   editButton.textContent = 'Add Site';
-   editButton.onclick = function () {
-      var newValue = prompt('Enter new site link');
-      if (newValue !== null) {
-         updateSites('sites', newValue);
-      }
-   };
-
-   div.insertBefore(editButton, header);
 });
 
 function updateField(fieldName, newValue) {
@@ -93,21 +42,5 @@ function updateField(fieldName, newValue) {
          }
       }
    };
-   xhr.send(JSON.stringify(data));
-}
-
-function updateSites(fieldName, newValue) {
-   var name = window.location.href;
-   name = decodeURIComponent(name);
-   name = name.substring(name.lastIndexOf('=') + 1);
-   var data = {
-      name: name,
-      field: fieldName,
-      value: newValue
-   };
-
-   var xhr = new XMLHttpRequest();
-   xhr.open('POST', '../php/tools/editModelSites.php', true);
-   xhr.setRequestHeader('Content-Type', 'application/json');
    xhr.send(JSON.stringify(data));
 }
