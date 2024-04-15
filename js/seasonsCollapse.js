12390c145244ec4d38e2d0b74d763594a2858157
GamingLegion <jacobdgrieco@gmail.com>
2024-04-14 23:45:05 -0400

    updated for seasons of a single series

--- js/seasonsCollapse.js
+++ js/seasonsCollapse.js
@@ -4,10 +4,16 @@ document.addEventListener("DOMContentLoaded", function () {
    if (collapsibles.length > 1) {
       collapsibles.forEach(function (div) {
          var collapsibleBtn = div.querySelector(".collapsible-btn");
+         var description = div.querySelector(".anime-description");
          var episodeSection = div.querySelector(".episode-section");
          var episodeItems = episodeSection.querySelectorAll(".episode");
 
          collapsibleBtn.addEventListener("click", function () {
+            if (description.style.display === "block") {
+               description.style.display = "none";
+            } else {
+               description.style.display = "block";
+            }
             if (episodeSection.style.display === "block") {
                episodeSection.style.display = "none";
             } else {
@@ -20,10 +26,12 @@ document.addEventListener("DOMContentLoaded", function () {
             });
          });
       });
-   } else {
+   } else if (collapsibles.length === 1) {
+      var description = document.querySelector(".anime-description");
       var episodeSection = document.querySelector(".episode-section");
       var episodeItems = episodeSection.querySelectorAll(".episode");
 
+      description.style.display = "block";
       episodeSection.style.display = "block";
 
       episodeItems.forEach(function (item, index) {
--- php/animePage.php
+++ php/animePage.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="../css/animePage.css">
-<link rel="stylesheet" type="text/css" href="../css/episode.css">
+<link rel="stylesheet" type="text/css" href="../css/seasons.css">
 <?php
 $IPATH = $_SERVER[ "DOCUMENT_ROOT" ] . "/PunkCloud/php/components/";
 include( $IPATH . "header.html" );
@@ -43,124 +43,91 @@ include( $IPATH . "header.html" );
             }
             mysqli_close( $connect );
             ?>
-         <script src="../js/titleOverflow.js"></script>
+            <script src="../js/titleOverflow.js"></script> 
          </div>
       </div>
       <div class="anime-body">
-         <div class="anime-description">
+         <div class="seasons">
             <?php
             $connect = mysqli_connect( 'localhost', 'root', 'theallseeingeyes', 'punkcloud' );
             $name = isset( $_GET[ 'link' ] ) ? $_GET[ 'link' ] : 'default';
-            $result = mysqli_query( $connect, "SELECT series, epi_num, status, start_date, end_date, air_season, brodcast, producers, licensors, studios, source, genres, demographic FROM anime_shows WHERE rom_name='" . $name . "'" );
-            $record = mysqli_fetch_assoc( $result );
+            $result = mysqli_query( $connect, "SELECT series, season, epi_num, start_date, end_date, air_season, brodcast, producers, licensors, studios FROM anime_shows WHERE series = '" . $name . "' ORDER BY season" );
+            while ( $record = mysqli_fetch_assoc( $result ) ) {
+               if ( $record[ 'season' ] !== NULL ) {
+                  echo '<div class="collapsible">';
+                  echo '<button class="collapsible-btn"><strong>' . $record[ 'season' ] . '</strong></button>';
+                  echo '<div id="collapsible">';
+                  echo '<div class="anime-description">';
 
-            echo '<div class="info_line">';
-            echo '<p><strong>Start Date:</strong></p>';
-            if ( $record[ 'start_date' ] !== NULL ) {
-               $start_date = new DateTime( $record[ 'start_date' ] );
-               $start_date = $start_date->format( 'F j, Y' );
-               echo '<p class="info" id="start_date">' . $start_date . '</p>';
-            } else {
-               echo '<p class="info" id="start_date"></p>';
+                  echo '<div class="info_line">';
+                  echo '<a><strong>Start Date:</strong></a>';
+                  if ( $record[ 'start_date' ] !== NULL ) {
+                     $start_date = new DateTime( $record[ 'start_date' ] );
+                     $start_date = $start_date->format( 'F j, Y' );
+                     echo '<a class="info" id="start_date">' . $start_date . '</a>';
+                  } else {
+                     echo '<a class="info" id="start_date"></a>';
+                  }
+                  echo '</div>';
+                  echo '<div class="info_line"">';
+                  echo '<a><strong>End Date:</strong></a>';
+                  if ( $record[ 'end_date' ] !== NULL ) {
+                     $end_date = new DateTime( $record[ 'end_date' ] );
+                     $end_date = $end_date->format( 'F j, Y' );
+                     echo '<a class="info" id="end_date">' . $end_date . '</a>';
+                  } else {
+                     echo '<a class="info" id="end_date"></a>';
+                  }
+                  echo '</div>';
+                  echo '<div class="info_line">';
+                  echo '<a><strong>Air Season:</strong></a>';
+                  echo '<a class="info" id="air_season">' . $record[ 'air_season' ] . '</a>';
+                  echo '</div>';
+                  echo '<div class="info_line">';
+                  echo '<a><strong>Brodcast:</strong></a>';
+                  echo '<a class="info" id="brodcsat">' . $record[ 'brodcast' ] . '</a>';
+                  echo '</div>';
+                  echo '<div class="info_line" id="prods">';
+                  echo '<a><strong>Producer(s):</strong></a>';
+                  $prods = $record[ 'producers' ];
+                  while ( strpos( $prods, ',' ) !== false ) {
+                     echo '<a class="info" id="producers">' . substr( $prods, 0, strpos( $prods, ',' ) + 2) . '</a>';
+                     $prods = substr( $prods, strpos( $prods, ',' ) + 2 );
+                  }
+                  echo '<a class="info" id="producers">' . $prods . '</a>';
+                  echo '</div>';
+                  echo '<div class="info_line">';
+                  echo '<a><strong>Licensor(s):</strong></a>';
+                  echo '<a class="info" id="licensors">' . $record[ 'licensors' ] . '' . '</a>';
+                  echo '</div>';
+                  echo '<div class="info_line">';
+                  echo '<a><strong>Studio:</strong></a>';
+                  echo '<a class="info" id="studios">' . $record[ 'studios' ] . '</a>';
+                  echo '</div>';
+                  echo '</div>';
+                  echo '<div class="episode-section">';
+                  for ( $i = 1; $i <= $record[ 'epi_num' ]; $i++ ) {
+                     echo '<div class="episode hidden">';
+                     echo '<div class="episode-thumbnail"> <img src="episode' . $i . '.jpg" alt="Episode ' . $i . '"> </div>';
+                     echo '<div class="episode-details">';
+                     echo '<div class="episode-info">';
+                     echo '<h3 class="episode-title">Episode ' . $i . '</h3>';
+                     echo '<button class="checkbox-btn"></button>';
+                     echo '</div>';
+                     echo '</div>';
+                     echo '</div>';
+                  }
+                  echo '</div>';
+                  echo '</div>';
+               }
             }
-            echo '</div>';
-            echo '<div class="info_line">';
-            echo '<p><strong>End Date:</strong></p>';
-            if ( $record[ 'end_date' ] !== NULL ) {
-               $end_date = new DateTime( $record[ 'end_date' ] );
-               $end_date = $end_date->format( 'F j, Y' );
-               echo '<p class="info" id="end_date">' . $end_date . '</p>';
-            } else {
-               echo '<p class="info" id="end_date"></p>';
-            }
-            echo '</div>';
-            echo '<div class="info_line">';
-            echo '<p><strong>Air Season:</strong></p>';
-            echo '<p class="info" id="air_season">' . $record[ 'air_season' ] . '</p>';
-            echo '</div>';
-            echo '<div class="info_line">';
-            echo '<p><strong>Brodcast:</strong></p>';
-            echo '<p class="info" id="brodcast">' . $record[ 'brodcast' ] . '</p>';
-            echo '</div>';
-            echo '<div class="info_line">';
-            echo '<p><strong>Producers:</strong></p>';
-            echo '<p class="info" id="producers">' . $record[ 'producers' ] . '</p>';
-            echo '</div>';
-            echo '<div class="info_line">';
-            echo '<p><strong>Licensors:</strong></p>';
-            echo '<p class="info" id="licensors">' . $record[ 'licensors' ] . '</p>';
-            echo '</div>';
-            echo '<div class="info_line">';
-            echo '<p><strong>Studios:</strong></p>';
-            echo '<p class="info" id="studios">' . $record[ 'studios' ] . '</p>';
-            echo '</div>';
-            echo '<div class="info_line">';
-            echo '<p><strong>Source:</strong></p>';
-            echo '<p class="info" id="source">' . $record[ 'source' ] . '</p>';
-            echo '</div>';
-            echo '<div class="info_line">';
-            echo '<p><strong>Genres:</strong></p>';
-            echo '<p class="info" id="genres">' . $record[ 'genres' ] . '</p>';
-            echo '</div>';
-            echo '<div class="info_line">';
-            echo '<p><strong>Demographic:</strong></p>';
-            echo '<p class="info" id="demographic">' . $record[ 'demographic' ] . '</p>';
-            echo '</div>';
-            mysqli_close( $connect );
             ?>
+            <script src="../js/editAnime.js"></script> 
          </div>
-         <script src="../js/editAnime.js"></script> 
-         <div class="seasons">
-            <div class="collapsible">
-               <button class="collapsible-btn"><strong>Season 1</strong></button>
-               <div class="episode-section">
-                  <?php
-                  $connect = mysqli_connect( 'localhost', 'root', 'theallseeingeyes', 'punkcloud' );
-                  $name = isset( $_GET[ 'link' ] ) ? $_GET[ 'link' ] : 'default';
-                  $result = mysqli_query( $connect, "SELECT epi_num FROM anime_shows WHERE rom_name='" . $name . "'" );
-                  $record = mysqli_fetch_assoc( $result );
-
-                  for ( $i = 1; $i <= $record[ 'epi_num' ]; $i++ ) {
-                     echo '<div class="episode hidden">';
-                     echo '<div class="episode-thumbnail"> <img src="episode' . $i . '.jpg" alt="Episode ' . $i . '"> </div>';
-                     echo '<div class="episode-details">';
-                     echo '<div class="episode-info">';
-                     echo '<h3 class="episode-title">Episode ' . $i . '</h3>';
-                     echo '<button class="checkbox-btn"></button>'; // Add the button here
-                     echo '</div>';
-                     echo '</div>';
-                     echo '</div>';
-                  }
-                  ?>
-               </div>
-            </div>
-            <div class="collapsible">
-               <button class="collapsible-btn"><strong>Season 2</strong></button>
-               <div class="episode-section">
-                  <?php
-                  $connect = mysqli_connect( 'localhost', 'root', 'theallseeingeyes', 'punkcloud' );
-                  $name = isset( $_GET[ 'link' ] ) ? $_GET[ 'link' ] : 'default';
-                  $result = mysqli_query( $connect, "SELECT epi_num FROM anime_shows WHERE rom_name='" . $name . "'" );
-                  $record = mysqli_fetch_assoc( $result );
-
-                  for ( $i = 1; $i <= $record[ 'epi_num' ]; $i++ ) {
-                     echo '<div class="episode hidden">';
-                     echo '<div class="episode-thumbnail"> <img src="episode' . $i . '.jpg" alt="Episode ' . $i . '"> </div>';
-                     echo '<div class="episode-details">';
-                     echo '<div class="episode-info">';
-                     echo '<h3 class="episode-title">Episode ' . $i . '</h3>';
-                     echo '<button class="checkbox-btn"></button>'; // Add the button here
-                     echo '</div>';
-                     echo '</div>';
-                     echo '</div>';
-                  }
-                  ?>
-               </div>
-            </div>
-         </div>
-         <script src="../js/episodeCollapse.js"></script> 
       </div>
+      <script src="../js/seasonsCollapse.js"></script> 
    </div>
+</div>
 </div>
 </body>
 </html>
