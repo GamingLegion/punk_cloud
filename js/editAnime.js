329dceff8f5d85280c66183d4736e8e3a01bbacc
GamingLegion <jacobdgrieco@gmail.com>
2024-04-26 22:40:34 -0400

    added edit button for admin

--- js/editAnime.js
+++ js/editAnime.js
@@ -9,11 +9,27 @@ infoLines.forEach(function (infoLine) {
    editButton.onclick = function () {
       var newValue = prompt('Enter new value for ' + firstParagraph.textContent, secondParagraph.textContent);
       if (newValue !== null) {
-         updateField(secondParagraph, secondParagraph.id, secondParagraph.getAttribute("data-season"),  newValue);
+         updateField(secondParagraph, secondParagraph.id, secondParagraph.dataset.season,  newValue);
       }
    };
 
    infoLine.insertBefore(editButton, firstParagraph);
+});
+var collapses = document.querySelectorAll(".collapsible");
+collapses.forEach(function(collapse) {
+   var imgDiv = collapse.querySelector(".imgDiv");
+   var img = imgDiv.querySelector("img");
+   var editButton = document.createElement('button');
+   editButton.id = 'editBtn';
+   editButton.textContent = 'Edit';
+   editButton.onclick = function () {
+      var newValue = prompt('Enter new value for image path');
+      if (newValue !== null) {
+         updateField(img, "image", img.dataset.season,  newValue);
+      }
+   };
+
+   imgDiv.insertBefore(editButton, img);
 });
 
 function updateField(line, fieldName, season, newValue) {
@@ -33,7 +49,11 @@ function updateField(line, fieldName, season, newValue) {
    xhr.setRequestHeader('Content-Type', 'application/json');
    xhr.onreadystatechange = function () {
       if (xhr.readyState === 4 && xhr.status === 200) {
-         line.textContent = newValue;
+         if(fieldName === "image") {
+            line.src = "http://localhost/PunkCloud/images/arts/anime/" + newValue;
+         } else {
+            line.textContent = newValue;
+         }
       }
    };
    xhr.send(JSON.stringify(data));
