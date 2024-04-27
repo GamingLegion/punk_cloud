c7f61e9e46834e9f854506b074dd52441a22f0a2
GamingLegion <jacobdgrieco@gmail.com>
2024-04-27 13:23:07 -0400

    added condition for blank value

--- php/tools/editAnime.php
+++ php/tools/editAnime.php
@@ -5,8 +5,7 @@ $data = json_decode( file_get_contents( 'php://input' ), true );
 
 date_default_timezone_set( 'America/New_York' );
 $upd_date = date( "Y-m-d H:i:s" );
-//$name = mysqli_real_escape_string($connect, $data['name']);
-$name='Tsuki ga Michibiku Isekai Douchuu';
+$name = mysqli_real_escape_string($connect, $data['name']);
 $season = mysqli_real_escape_string($connect, $data['season']);
 $field = mysqli_real_escape_string($connect, $data['field']);
 $value = mysqli_real_escape_string($connect, $data['value']);
@@ -15,10 +14,17 @@ if ( $field == 'start_date' || $field == 'end_date' ) {
    $value = date( "Y-m-d", strtotime( $value ) );
 }
 
-$updateStmt = mysqli_prepare($connect, "UPDATE anime SET $field = ?, upd_date = ? WHERE rom_name = ? AND season = ?");
-mysqli_stmt_bind_param($updateStmt, 'ssss', $value, $upd_date, $name, $season);
-mysqli_stmt_execute($updateStmt);
-mysqli_stmt_close($updateStmt);
+if($value == '' || $value == NULL) {
+   $updateStmt = mysqli_prepare($connect, "UPDATE anime SET $field = NULL, upd_date = ? WHERE rom_name = ? AND season = ?");
+   mysqli_stmt_bind_param($updateStmt, 'sss', $upd_date, $name, $season);
+   mysqli_stmt_execute($updateStmt);
+   mysqli_stmt_close($updateStmt);
+} else {
+   $updateStmt = mysqli_prepare($connect, "UPDATE anime SET $field = ?, upd_date = ? WHERE rom_name = ? AND season = ?");
+   mysqli_stmt_bind_param($updateStmt, 'ssss', $value, $upd_date, $name, $season);
+   mysqli_stmt_execute($updateStmt);
+   mysqli_stmt_close($updateStmt);
+}
 if($field == 'season') {
     $updateStmt2 = mysqli_prepare($connect2, "UPDATE anime SET anime_season = ? WHERE anime_name = ? AND anime_season = ?");
     mysqli_stmt_bind_param($updateStmt2, 'sss', $value, $name, $season);
