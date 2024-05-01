f03c21632f5f03e1c543a4c024226b23563d7d66
GamingLegion <jacobdgrieco@gmail.com>
2024-05-01 18:34:43 -0400

    updated

--- js/animePageJS/editAnimeEpi.js
+++ js/animePageJS/editAnimeEpi.js
@@ -56,10 +56,10 @@ function updateOverField(pmaField, newValue, fieldName, button) {
    xhr.onreadystatechange = function () {
       if (xhr.readyState === 4 && xhr.status === 200) {
          var field = overlay.querySelector("#" + fieldName);
-         var btn = searchEpi(checkbox);
+         var btn = searchEpi(checkbox).closest('.episode');
          if (fieldName === 'overlayImg') {
             field.querySelector('img').src = '/PunkCloud/images/episodes/anime/' + newValue;
-            btn.closest('.episode').querySelector('img').src = '/PunkCloud/images/episodes/anime/' + newValue;
+            btn.querySelector('img').src = '/PunkCloud/images/episodes/anime/' + newValue;
          } else {
             field.textContent = newValue;
          }
