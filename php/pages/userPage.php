34ac05666246df3b30ae852e5440776f376338ff
GamingLegion <jacobdgrieco@gmail.com>
2024-05-01 20:39:18 -0400

    created an actual user page

--- php/pages/userPage.php
+++ php/pages/userPage.php
@@ -29,46 +29,26 @@ if ( isset( $_SESSION[ 'user' ] ) ) {
    <div class="account">
       <div class="info">
          <?php
-         $query = "SELECT username, icon FROM users WHERE username = '" . $_SESSION[ 'user' ] . "'";
-         $result = mysqli_query( $connect1, $query );
+         $result = mysqli_query( $connect1, "SELECT icon FROM users WHERE username = '" . $_SESSION[ 'user' ] . "'" );
          $record = mysqli_fetch_assoc( $result );
-         echo '<a class="username">' . $record[ 'username' ] . '</a>';
-         echo '<img class="userIcon" src="' . $userIcons . $record[ 'icon' ] . '" alt="User Icon">';
+         echo '<a class="username">' . $_SESSION[ 'user' ] . '</a>';
+         echo '<div class="userIcon">';
+         echo '<img  src="' . $userIcons . $record[ 'icon' ] . '" alt="User Icon">';
+         echo '</div>';
          ?>
       </div>
       <div class="lists">
          <div class="list allLists">
-            <p>All Lists</p>
-            <?php
-            $result = mysqli_query( $connect1, 'Select eng_name, rom_name, image, series FROM anime ORDER BY ins_date DESC LIMIT 9' );
-
-            while ( $record = mysqli_fetch_assoc( $result ) ) {
-               echo '<div class="aniCard" id="card" width="50%">';
-               echo '<div id="thumbnail">';
-               echo '<img src="' . $animeArts . $record[ 'image' ] . '" alt="' . $record[ 'rom_name' ] . '">';
-               echo '<div class="title">';
-               if ( $record[ 'eng_name' ] != NULL ) {
-                  echo '<p title="' . $record[ 'series' ] . '">' . $record[ 'eng_name' ] . '</p>';
-               } else {
-                  echo '<p title="' . $record[ 'series' ] . '">' . $record[ 'rom_name' ] . '</p>';
-               }
-               echo '</div>';
-               echo '</div>';
-               echo '</div>';
-            }
-            ?>
-         </div>
-         <div class="filmLists">
-            <div class="list anime">
-               <p>Anime List</p>
+            <p class="listName">All Lists</p>
+            <div class="listCards">
                <?php
                $last = array();
                $result = mysqli_query( $connect3, "SELECT anime_name, anime_season FROM " . $_SESSION[ 'user' ] . " ORDER BY ins_date DESC" );
                while ( $record = mysqli_fetch_assoc( $result ) ) {
-                  if(count($last) >= 4) {
+                  if ( count( $last ) >= 5 ) {
                      break;
                   }
-                  if ( !in_array($record[ 'anime_name' ] . ' ' . $record[ 'anime_season' ], $last) ) {
+                  if ( !in_array( $record[ 'anime_name' ] . ' ' . $record[ 'anime_season' ], $last ) ) {
                      $result2 = mysqli_query( $connect1, "SELECT image, rom_name, eng_name, series FROM anime WHERE rom_name = '" . $record[ 'anime_name' ] . "' AND season = '" . $record[ 'anime_season' ] . "'" );
                      $record2 = mysqli_fetch_assoc( $result2 );
                      echo '<div class="aniCard" id="card" width="50%">';
@@ -84,41 +64,86 @@ if ( isset( $_SESSION[ 'user' ] ) ) {
                      echo '</div>';
                      echo '</div>';
 
-                     array_push($last, $record[ 'anime_name' ] . ' ' . $record[ 'anime_season' ]);
+                     array_push( $last, $record[ 'anime_name' ] . ' ' . $record[ 'anime_season' ] );
                   }
                }
                ?>
             </div>
+         </div>
+         <div class="filmLists">
+            <div class="list anime">
+               <p class="listName">Anime List</p>
+               <div class="listCards">
+                  <?php
+                  $last = array();
+                  $result = mysqli_query( $connect3, "SELECT anime_name, anime_season FROM " . $_SESSION[ 'user' ] . " ORDER BY ins_date DESC" );
+                  while ( $record = mysqli_fetch_assoc( $result ) ) {
+                     if ( count( $last ) >= 5 ) {
+                        break;
+                     }
+                     if ( !in_array( $record[ 'anime_name' ] . ' ' . $record[ 'anime_season' ], $last ) ) {
+                        $result2 = mysqli_query( $connect1, "SELECT image, rom_name, eng_name, series FROM anime WHERE rom_name = '" . $record[ 'anime_name' ] . "' AND season = '" . $record[ 'anime_season' ] . "'" );
+                        $record2 = mysqli_fetch_assoc( $result2 );
+                        echo '<div class="aniCard" id="card" width="50%">';
+                        echo '<div id="thumbnail">';
+                        echo '<img src="' . $animeArts . $record2[ 'image' ] . '" alt="' . $record2[ 'rom_name' ] . '">';
+                        echo '<div class="title">';
+                        if ( $record2[ 'eng_name' ] != NULL ) {
+                           echo '<p title="' . $record2[ 'series' ] . '">' . $record2[ 'eng_name' ] . '</p>';
+                        } else {
+                           echo '<p title="' . $record2[ 'series' ] . '">' . $record2[ 'rom_name' ] . '</p>';
+                        }
+                        echo '</div>';
+                        echo '</div>';
+                        echo '</div>';
+
+                        array_push( $last, $record[ 'anime_name' ] . ' ' . $record[ 'anime_season' ] );
+                     }
+                  }
+                  ?>
+               </div>
+            </div>
             <div class="list liveaction">
-               <p>Live Action List</p>
+               <p class="listName">Live Action List</p>
+               <div class="listCards"> </div>
             </div>
             <div class="list cartoon">
-               <p>Cartoon List</p>
+               <p class="listName">Cartoon List</p>
+               <div class="listCards"> </div>
             </div>
          </div>
+         <br><br><br>
          <div class="textLists">
             <div class="list novel">
-               <p>Novel List</p>
+               <p class="listName">Novel List</p>
+               <div class="listCards"> </div>
             </div>
             <div class="list comic">
-               <p>Comic List</p>
+               <p class="listName">Comic List</p>
+               <div class="listCards"> </div>
             </div>
             <div class="list manga">
-               <p>Manga List</p>
+               <p class="listName">Manga List</p>
+               <div class="listCards"> </div>
             </div>
             <div class="list manhwa">
-               <p>Manhwa List</p>
+               <p class="listName">Manhwa List</p>
+               <div class="listCards"> </div>
             </div>
             <div class="list manhua">
-               <p>Manhua List</p>
+               <p class="listName">Manhua List</p>
+               <div class="listCards"> </div>
             </div>
             <div class="list lightnovel">
-               <p>Light Novel List</p>
+               <p class="listName">Light Novel List</p>
+               <div class="listCards"> </div>
             </div>
             <div class="list webnovel">
-               <p>Web Novel List</p>
+               <p class="listName">Web Novel List</p>
+               <div class="listCards"> </div>
             </div>
          </div>
+      <br>
       </div>
       <?php
       echo '<script src="' . $js . 'pageClick.js"></script>';
